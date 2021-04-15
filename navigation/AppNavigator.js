29deaa0fa6f5143e56519f99b53f1ce89392375f
George Codat Tudoran <office@solcreation.ro>
2021-04-15 20:39:25 +0300

    establishing the project structure

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,4 @@
+import React from 'react';
+
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator, DrawerItemList } from '@react-navigation/drawer';
---
+++ store/StartupScreen.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+const StartupScreen = () => {
+    return (
+        <View>Primul ecran unde vor fi desenate boxurile cu categoriile care vor duce la produse ca si pe web</View>
+    )
+}
